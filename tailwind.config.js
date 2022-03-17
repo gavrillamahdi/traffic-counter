0b0aa25cbd932c74f2133679992dd573e061e87f
Gavrilla Mahdi Ghozali <gavrillamahdi@gmail.com>
2022-03-17 11:58:43 +0700

    config tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        qs: [
+          'Quicksand',
+          'ui-sans-serif',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          '"Noto Sans"',
+          'sans-serif',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+        ],
+      },
+    },
+  },
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: ['garden', 'dracula'],
+  },
+};
