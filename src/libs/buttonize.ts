1b5c1d04d06d7dd37a36081e57561b868ffef92c
Gavrilla Mahdi Ghozali <gavrillamahdi@gmail.com>
2022-03-18 19:20:57 +0700

    fix buttonize onKeyDown function

--- src/libs/buttonize.ts
+++ src/libs/buttonize.ts
@@ -1,15 +1,17 @@
 interface buttonizeProps {
   role: 'button';
+  tabIndex: number;
   onClick: () => void;
-  onKeyDown: (event: React.KeyboardEvent) => void;
+  onKeyDown: (event: React.KeyboardEvent) => void; // eslint-disable-line
 }
 
 export default function buttonize(callback: () => void): buttonizeProps {
   return {
     role: 'button',
+    tabIndex: 0,
     onClick: callback,
     onKeyDown: (event: React.KeyboardEvent) => {
-      if (event.key === '13' || event.key === '32') callback();
+      if (event.key === ' ' || event.key === 'Enter') (event.target as HTMLElement).click();
     },
   };
 }
