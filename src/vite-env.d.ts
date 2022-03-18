9814384ae065b78f544263af5204dd12255664e9
Gavrilla Mahdi Ghozali <gavrillamahdi@gmail.com>
2022-03-18 09:01:14 +0700

    declare theme-change module since no @types/module given

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,8 @@
+/* eslint-disable */
 /// <reference types="vite/client" />
+
+declare module 'theme-change' {
+  export function themeChange(params: boolean): void;
+}
+
+declare module '@/libs/buttonize';
