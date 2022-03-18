e1f6162e5ea0d90655d4b0f1aa25323f16057e1f
Gavrilla Mahdi Ghozali <gavrillamahdi@gmail.com>
2022-03-18 19:19:50 +0700

    fix buttonize declare

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,4 +5,10 @@ declare module 'theme-change' {
   export function themeChange(params: boolean): void;
 }
 
-declare module '@/libs/buttonize';
+declare module '@/libs/buttonize' {
+  export default function buttonize(callback: () => void): {
+    role: 'button';
+    onClick: () => void;
+    onKeyDown: (event: React.KeyboardEvent) => void;
+  };
+}
