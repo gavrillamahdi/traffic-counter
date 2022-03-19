510a4798e081f510e5c715bee508c318f6beedef
Gavrilla Mahdi Ghozali <gavrillamahdi@gmail.com>
2022-03-19 23:24:52 +0700

    refactor modul declaration

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,14 +1,1 @@
-/* eslint-disable */
 /// <reference types="vite/client" />
-
-declare module 'theme-change' {
-  export function themeChange(params: boolean): void;
-}
-
-declare module '@/libs/buttonize' {
-  export default function buttonize(callback: () => void): {
-    role: 'button';
-    onClick: () => void;
-    onKeyDown: (event: React.KeyboardEvent) => void;
-  };
-}
