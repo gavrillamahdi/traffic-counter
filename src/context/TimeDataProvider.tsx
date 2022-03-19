94c847e4d482fef05353e957c3a76b6e7b014f8b
Gavrilla Mahdi Ghozali <gavrillamahdi@gmail.com>
2022-03-19 20:20:08 +0700

    refactor context timedata

--- src/context/TimeDataProvider.tsx
+++ src/context/TimeDataProvider.tsx
@@ -6,6 +6,11 @@ interface TimeDataProviderProps {
   children: React.ReactNode;
 }
 
+interface TimeDataContextType {
+  timeData: TimeData;
+  setTimeData?: (timeData: TimeData) => void;
+}
+
 const TIME: Time = {
   hour: 0,
   minute: 0,
@@ -14,12 +19,14 @@ const TIME: Time = {
 
 const TIME_DATA = { start: TIME, end: TIME, interval: TIME };
 
-const TimeDataContext = React.createContext<TimeData>(TIME_DATA);
+const TimeDataContext = React.createContext<TimeDataContextType>({
+  timeData: TIME_DATA,
+});
 
 export function TimeDataProvider({ children }: TimeDataProviderProps): JSX.Element {
   const [timeData, setTimeData] = useState<TimeData>(TIME_DATA);
 
-  const contextVal = React.useMemo(() => ({ ...timeData, setTimeData }), [timeData]);
+  const contextVal = React.useMemo(() => ({ timeData, setTimeData }), [timeData]);
 
   return <TimeDataContext.Provider value={contextVal}>{children}</TimeDataContext.Provider>;
 }
