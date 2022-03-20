08e6807a5ed284749ebad1914d604fd0300093ed
Gavrilla Mahdi Ghozali <gavrillamahdi@gmail.com>
2022-03-21 00:52:19 +0700

    remove unused comments

--- src/libs/saveExcel.ts
+++ src/libs/saveExcel.ts
@@ -2,24 +2,6 @@ import Excel from 'exceljs';
 import { saveAs } from 'file-saver';
 import { ColumnType, RowType } from '@/types/ExcelJsType';
 
-// const COLUMN: ColumnType[] = [
-//   { header: 'No', key: 'no' },
-//   { header: 'Waktu', key: 'waktu' },
-//   { header: 'MC', key: 'mc' },
-//   { header: 'LV', key: 'lv' },
-//   { header: 'HV', key: 'hv' },
-// ];
-
-// const data: string[] = ['12', '13', '14', '15'];
-
-// const row: RowType[] = data.map((item, index) => ({
-//   no: index + 1,
-//   waktu: item,
-//   mc: 0,
-//   lv: 0,
-//   hv: 0,
-// }));
-
 export default async function saveExcel(columns: ColumnType[], rows: RowType[]) {
   const workbook = new Excel.Workbook();
   const worksheet = workbook.addWorksheet('Data');
