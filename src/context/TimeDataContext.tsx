4c27d5150ab25b494005f9bb91d20a464c640172
Gavrilla Mahdi Ghozali <gavrillamahdi@gmail.com>
2022-03-22 17:26:00 +0700

    remove unused var

--- src/context/TimeDataContext.tsx
+++ src/context/TimeDataContext.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import useLocalStorage from '@/hooks/useLocalStorage';
 import { TimeData, Time } from '@/types/TimeDataType';
