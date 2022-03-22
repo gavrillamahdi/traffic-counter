0590b856e77821f36be0260fa60b9c7fc22fad2c
Gavrilla Mahdi Ghozali <gavrillamahdi@gmail.com>
2022-03-22 20:39:35 +0700

    make new input validation

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -18,10 +18,9 @@ export default function Input({
 }: InputProps): JSX.Element {
   const { timeData, setTimeData } = useTimeDataContext();
 
-  const [inputValue, setInputValue] = React.useState<{ value: string; zeroOccurance: number }>({
-    value: timeData[section][fraction] ? String(timeData[section][fraction]) : '',
-    zeroOccurance: 0,
-  });
+  const [inputValue, setInputValue] = React.useState<string>(
+    timeData[section][fraction] ? String(timeData[section][fraction]).padStart(2, '0') : '00'
+  );
 
   return (
     <label htmlFor={id} className="input-group" key={id}>
@@ -29,58 +28,20 @@ export default function Input({
         id={id}
         type="number"
         className="input input-bordered text-center"
-        value={inputValue.value.padStart(2, '0')}
-        onChange={({ target: { value }, nativeEvent }) => {
-          if ((nativeEvent as any).data === '0') {
-            const zeroOccurance = inputValue.zeroOccurance + 1;
-            setInputValue((prev) => ({
-              ...prev,
-              zeroOccurance: prev.zeroOccurance + 1,
-            }));
+        value={inputValue}
+        onChange={({ target: { value } }) => {
+          if (+value < (fraction === 'hour' ? 24 : 60) && +value >= 0) setInputValue(value);
+          else setInputValue((prev) => prev);
 
-            if (zeroOccurance === 2 && nextNode) {
-              nextNode.focus();
-              setInputValue((prev) => ({
-                ...prev,
-                zeroOccurance: 0,
-              }));
-              setTimeData((prev) => ({
-                ...prev,
-                [section]: { ...prev[section], [fraction]: value ? +value : 0 },
-              }));
-              return;
-            }
-          }
-
-          if (+value < (fraction === 'hour' ? 24 : 60) && +value >= 0) {
-            setInputValue((prev) => ({ ...prev, value: value.replace(/0+/, '') }));
-            setTimeData((prev) => ({
-              ...prev,
-              [section]: { ...prev[section], [fraction]: +value },
-            }));
-          }
-
-          if (
-            value.length === 2 ||
-            +value > (fraction === 'hour' ? 2 : 5) ||
-            (!value.endsWith('0') && inputValue.zeroOccurance !== 0)
-          ) {
+          if (value.length === 2 || +value > (fraction === 'hour' ? 2 : 5)) {
             if (nextNode) nextNode.focus();
-            setInputValue((prev) => ({
-              ...prev,
-              zeroOccurance: 0,
-            }));
-            setTimeData((prev) => ({
-              ...prev,
-              [section]: { ...prev[section], [fraction]: value ? +value : 0 },
-            }));
           }
         }}
         onKeyDown={(e) => {
           if (['e', '+', '-'].includes(e.key)) e.preventDefault();
           else if (['Backspace', 'Delete'].includes(e.key)) {
             e.preventDefault();
-            setInputValue((prev) => ({ ...prev, value: '0' }));
+            setInputValue('');
             setTimeData((prev) => ({
               ...prev,
               [section]: { ...prev[section], [fraction]: 0 },
@@ -88,6 +49,20 @@ export default function Input({
           }
         }}
         onFocus={(e) => e.target.select()}
+        onBlur={({ target: { value } }) => {
+          if (+value < (fraction === 'hour' ? 24 : 60) && +value >= 0) {
+            setInputValue(value.padStart(2, '0'));
+          } else {
+            setInputValue((prev) => prev.padStart(2, '0'));
+            return;
+          }
+
+          if (value.length > 2) setInputValue(value.slice(-2));
+          setTimeData((prev) => ({
+            ...prev,
+            [section]: { ...prev[section], [fraction]: +value },
+          }));
+        }}
       />
       <span title={fraction}>{spanContent}</span>
     </label>
